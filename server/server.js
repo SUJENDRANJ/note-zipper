26592ecdb9473f8cf40084de9d1250f19bbae529
Sujendran <sujendranjanagiraman.com>
2026-02-02 16:35:09 +0530

    path resolve

--- server/server.js
+++ server/server.js
@@ -25,6 +25,24 @@ app.use("/api/notes", noteRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+// --------------------------deployment------------------------------
+const path = require("path");
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html")),
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
+
 connectDB()
   .then(() => {
     app.listen(5000, () => {
