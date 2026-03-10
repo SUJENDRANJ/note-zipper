e06a03f85e89fa4cac4f831d1d70260de499a227
Sujendran <sujendranjanagiraman.com>
2026-03-10 12:38:34 +0530

    small correction

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/shared/Footer";
+import Header from "../components/shared/Header";
 import { useRouteError } from "react-router-dom";
 
 const Error = () => {
@@ -6,11 +8,15 @@ const Error = () => {
   console.log(error);
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h2 className="text-2xl font-bold">
-        OOPS! <span>{error?.status || "404"}</span>{" "}
-        {error?.data || "Something went wrong"}
-      </h2>
+    <div>
+      <Header />
+      <div className="flex items-center justify-center h-[80vh]">
+        <h2 className="text-2xl font-bold">
+          OOPS! <span>{error?.status || "404"}</span>{" "}
+          {error?.data || "Something went wrong"}
+        </h2>
+      </div>
+      <Footer />
     </div>
   );
 };
